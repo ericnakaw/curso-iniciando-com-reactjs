2a791cad63e3d841d69458cd3efafe6a7de3ae04
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-08-21 18:34:40 -0300

    State e seu ciclo de vida

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 
+import StateInput from './StateInput'
+
 class HelloWorld extends Component
 {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name: 'Eric Nakawatase'
+        }
+    }
+
     render() {
+        const result = 4
+        const style = {
+            color: '#ff0000',
+            fontSize: 50,
+            backgroundColor: '#000000'
+        }
+
         return (
             <div className="my-class-from-react">
-                <h1>Hello World from Eric</h1>
+                {/*Este é o meu primeiro comentário*/}
+                <StateInput />
+                <h1 style={style}>Hello World from Eric</h1>
+                <h4>{ result === 2 * 2 ? <div><ul><li>TRUE</li></ul></div> : <div><ul><li>FALSE</li></ul></div> }</h4>
+                <span>{this.state.name}</span>
             </div>
         )   
     }
