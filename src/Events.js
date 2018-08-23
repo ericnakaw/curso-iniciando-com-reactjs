3772f5f58a9c9ad7dbe26be29ded459764c08a09
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-08-23 16:49:58 -0300

    Trabalhado com eventos - Finalizando

--- src/Events.js
+++ src/Events.js
@@ -9,7 +9,8 @@ class Events extends Component {
             data: []
         }
         this.updateInputName = this.updaateInputName.bind(this)
-        this.clickEvent = this.clickEvent.bind(this)
+        this.clickEvent      = this.clickEvent.bind(this)
+        this.deleteEvent     = this.deleteEvent.bind(this)
     }
 
     updaateInputName(e){
@@ -25,10 +26,24 @@ class Events extends Component {
         })
     }
 
+    deleteEvent(item){
+        const newState = this.state.data
+        if(newState.indexOf(item) > -1){
+            newState.splice(newState.indexOf(item),1)
+            this.setState({
+                data: newState
+            })
+        }
+        
+    }
+
     render(){
         const { data } = this.state
         const itens = data.map((item) => {
-                return <li key={item}>{item.toString()}</li>
+                return  <li key={item}>
+                            {item.toString()}
+                            <button type="button" onClick={this.deleteEvent.bind(this,item)}>Delete</button>
+                        </li>
             })
 
         return(
